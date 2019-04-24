433d3fa39f35724c8e5416717af2c287b186b611
roy1210 <rswkure@gmail.com>
2019-04-25 06:28:12 +0800

    landing page

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -1,10 +1,47 @@
 import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 
 class Landing extends Component {
   render() {
     return (
-      <div>
-        <h1>Landing Page</h1>
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
+              alt="avatar"
+              className="avatar-img"
+            />
+            <div className="banner-text">
+              <h1>Full Stack Web Developer</h1>
+
+              <hr />
+
+              <p>
+                HTML/CSS | Bootstrap | JavaScript | React | React Native |
+                NodeJS | Express | MongoDB
+              </p>
+              <div className="social-links">
+                {/* LinkedIn */}
+                <a
+                  href="https://google.com"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i class="fab fa-linkedin" aria-hidden="true" />
+                </a>
+                {/* Github */}
+                <a
+                  href="https://google.com"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i class="fab fa-github" aria-hidden="true" />
+                </a>
+              </div>
+            </div>
+          </Cell>
+        </Grid>
       </div>
     );
   }
--- src/components/main.js
+++ src/components/main.js
@@ -8,15 +8,19 @@ import Projects from "./projects";
 import Resume from "./resume";
 
 // no need class for `stateless`
+// exact pass for default page
+// can omit `const Main = () => { return () } ` as `const Main =() => ( <Switch></Switch> ); `
 
-const Main = () => (
-  <Switch>
-    <Route exact path="/" component={LandingPage} />
-    <Route path="/aboutme" component={AboutMe} />
-    <Route path="/contact" component={Contact} />
-    <Route path="/projects" component={Projects} />
-    <Route path="/resume" component={Resume} />
-  </Switch>
-);
+const Main = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={LandingPage} />
+      <Route path="/aboutme" component={AboutMe} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/projects" component={Projects} />
+      <Route path="/resume" component={Resume} />
+    </Switch>
+  );
+};
 
 export default Main;
