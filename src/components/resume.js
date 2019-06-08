6ee199571e395a34acc472d40a8d0a7e73bb542f
roy1210 <42575132+roy1210@users.noreply.github.com>
2019-06-08 22:15:02 +0800

    Self introduce

--- src/components/resume.js
+++ src/components/resume.js
@@ -28,14 +28,13 @@ class Resume extends Component {
               <p>
                 My name is Roy.
                 <br />
-                I'm a happy learner as I love to touch new technology every day.
+                I'm a happy learner and I love to learn about new technology
+                every day.
+                <br />
+                I communicate well with people from different culutures.
                 <br />
                 I'd like to dive into the tech industry because this is the
                 place to use technology to make the world a better place!
-                <br />
-                <br />
-                Other than programing language, I can speak Japanese, Chinese
-                and English as I have lived in Japan, China and Singapore.
                 <br />
                 Now, I'm ready to contribute as a software developer in the
                 global environment.
@@ -43,13 +42,16 @@ class Resume extends Component {
             </div>
             <div className="contact">
               <hr style={{ borderTop: "3px solid #833fb2", width: "100%" }} />
-              <h5>Location:</h5>
-              <p>Singapore</p>
-              <h5>Skype:</h5>
-              <p>roy_shoe_wu</p>
-              <h5>Email:</h5>
+              <i className="fas fa-map-marked-alt" aria-hidden="true" />
+              <span>Singapore</span>
+              <br />
+              <i className="fab fa-skype" aria-hidden="true" />
+              <span> roy_shoe_wu</span>
+              <br />
+              <i className="fa fa-envelope" aria-hidden="true" />
               <a href="mailto:rswkure@gmail.com">rswkure@gmail.com</a>
-              <h5>LinkedIn:</h5>
+              <br />
+              <i class="fab fa-linkedin" aria-hidden="true" />
               <a
                 href="https://www.linkedin.com/in/roy1210/"
                 rel="noopener noreferrer"
@@ -57,7 +59,8 @@ class Resume extends Component {
               >
                 https://www.linkedin.com/in/roy1210/
               </a>
-              <h5>Github:</h5>
+              <br />
+              <i class="fab fa-github" aria-hidden="true" />
               <a
                 href="https://github.com/roy1210"
                 rel="noopener noreferrer"
@@ -92,7 +95,7 @@ class Resume extends Component {
               <h4>Knowledge / Languages</h4>
               <SkillsBuffer skill="Blockchain" progress={75} />
               <Skills skill="Japanese" progress={100} />
-              <Skills skill="Chinese" progress={90} />
+              <Skills skill="Chinese" progress={85} />
               <Skills skill="English" progress={70} />
               <p>TOEIC score: 875</p>
               <a
