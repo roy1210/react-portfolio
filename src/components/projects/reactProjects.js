92f00629f86eb002b31f757b2504c0ec169c5fb5
roy1210 <42575132+roy1210@users.noreply.github.com>
2019-06-10 22:02:38 +0800

    style={{alignItems:"center"}}

--- src/components/projects/reactProjects.js
+++ src/components/projects/reactProjects.js
@@ -15,7 +15,7 @@ class ReactProjects extends Component {
     return (
       <div>
         <Grid className="projects-grid">
-          <Cell phone={9}>
+          <Cell phone={9} style={{ alignItems: "center" }}>
             {/* Project 1 */}
             <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
               <CardTitle
@@ -71,7 +71,7 @@ class ReactProjects extends Component {
           </Cell>
 
           {/* Project2  */}
-          <Cell phone={9}>
+          <Cell phone={9} style={{ alignItems: "center" }}>
             <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
               <CardTitle
                 style={{
