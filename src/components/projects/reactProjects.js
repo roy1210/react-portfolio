884fb5e36c15a5b40782f50f5822bf6bfacc688f
roy1210 <42575132+roy1210@users.noreply.github.com>
2019-06-16 11:56:48 +0800

    Update project gif

--- src/components/projects/reactProjects.js
+++ src/components/projects/reactProjects.js
@@ -9,7 +9,7 @@ class ReactProjects extends Component {
         <Grid className="projects-grid">
           <Cell col={4} phone={12}>
             <Project
-              photoUrl="https://github.com/roy1210/JS_React_Portfolio/blob/master/src/img/React_PF/Portfolio.gif?raw=true"
+              photoUrl="https://github.com/roy1210/JS_React_Portfolio/blob/master/src/img/React_PF/190616Portfolio.gif?raw=true"
               name="Portfolio"
               detail1="This portfolio website."
               detail2="Created with React Router, Semantic-ui-react, and React-MDL."
