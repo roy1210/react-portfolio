1f58b5d5e588a71897a5e35136b5afd743926a8f
roy1210 <42575132+roy1210@users.noreply.github.com>
2019-06-30 22:13:31 +0800

    Add "Made with React.js" footer

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -1,6 +1,24 @@
 import React from "react";
 
 const thisYear = new Date().getFullYear();
-const AppFooter = () => <small>© {thisYear} Shoe Kure</small>;
+const AppFooter = () => (
+  <div className="AppFooter">
+    <small>© {thisYear} Shoe Kure</small>
+    <br />
+    <span className="AppFooter-made-with">Made with</span>
+
+    <span className="AppFooter-react">
+      <a
+        href="https://codesandbox.io/s/my-portfolio-qmqcp"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <i className="fab fa-react" />
+        {"\u00A0"}
+        React.js
+      </a>
+    </span>
+  </div>
+);
 
 export default AppFooter;
