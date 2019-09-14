200a02da9455e06ca9b183785fb0a95d1c03f9bb
Shoe Kure <rswkure@gmail.com>
2019-09-14 10:11:35 +0800

    Add aria-label in LandingIcon to improve accessibility

--- src/components/LandingPage/LandingIcon.js
+++ src/components/LandingPage/LandingIcon.js
@@ -2,7 +2,12 @@ import React from "react";
 
 const LandingIcon = ({ url, icon }) => (
   <div className="Landing-social-links">
-    <a href={url} rel="noopener noreferrer" target="_blank">
+    <a
+      href={url}
+      rel="noopener noreferrer"
+      target="_blank"
+      aria-label="Go to my social links"
+    >
       <i className={icon} aria-hidden="true" />
     </a>
   </div>
--- src/components/Projects/ReactProjects.js
+++ src/components/Projects/ReactProjects.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { selectReactProjects } from '../../redux/Projects/projects.selector';
-import { Grid, Cell } from 'react-mdl';
-import Project from './Project';
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectReactProjects } from "../../redux/Projects/projects.selector";
+import { Grid, Cell } from "react-mdl";
+import Project from "./Project";
 
 const ReactProjects = ({ projects }) => {
   return (
-    <Grid className='projects-grid'>
+    <Grid className="projects-grid">
       {/* { ...otherSectionProps }: One of the Destructuring assignment. Same key args. Eg. `name = {name}` */}
       {projects.map(({ ...otherSectionProps }, index) => (
         <Cell col={4} phone={12}>
