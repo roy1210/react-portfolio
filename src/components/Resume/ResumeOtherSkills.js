606300dfc2b42ef9731a299e69f5e3706a2b8582
Shoe Kure <rswkure@gmail.com>
2019-09-22 16:09:40 +0800

    Refactoring OtherBackendSkills

--- src/components/Resume/ResumeOtherSkills.js
+++ src/components/Resume/ResumeOtherSkills.js
@@ -1,55 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
 import { Table } from "semantic-ui-react";
 
-const ResumeOtherBackendSkills = () => (
-  <div className="Resume-other-than-that">
-    <h5 className="Resume-backend-other">Other than above</h5>
-    <div style={{ fontSize: "1.1rem", margin: "0.3rem" }}>
-      <Table celled inverted>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell width={4}>Skills</Table.HeaderCell>
-            <Table.HeaderCell width={12}>Description</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
+class ResumeOtherBackendSkills extends Component {
+  skillsets = [
+    {
+      skill: "SSH",
+      description: "Remote linux server and generate ssh keys (DigitalOcean)"
+    },
+    {
+      skill: "Website Optimization",
+      description:
+        "Picture resize, Code splitting, Load balancing, Caching and Code optimizations"
+    },
+    {
+      skill: "Progressive Web App",
+      description: "Create PWA that works offline like a native mobile app"
+    },
+    {
+      skill: "Secure",
+      description:
+        "HTTPS, Code secrets (.gitignore), Secure headers (Helmet), Encrypt"
+    },
+    {
+      skill: "Docker",
+      description:
+        "Use dockerfile and docker-compose to create containers for services"
+    }
+  ];
+  render() {
+    return (
+      <div className="Resume-other-than-that">
+        <h5 className="Resume-backend-other">Other than above</h5>
+        <div style={{ fontSize: "1.1rem", margin: "0.3rem" }}>
+          <Table celled inverted>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell width={4}>Skill</Table.HeaderCell>
+                <Table.HeaderCell width={12}>Description</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
 
-        <Table.Body>
-          <Table.Row>
-            <Table.Cell>SSH</Table.Cell>
-            <Table.Cell>
-              Remote linux server and generate ssh keys (DigitalOcean)
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>Website Optimization</Table.Cell>
-            <Table.Cell>
-              Picture resize, Code splitting, Load balancing, Caching and Code
-              optimizations
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>Progressive Web App</Table.Cell>
-            <Table.Cell>
-              Create PWA that works offline like a native mobile app
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>Secure</Table.Cell>
-            <Table.Cell>
-              HTTPS, Code secrets (.gitignore), Secure headers (Helmet), Encrypt
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>Docker</Table.Cell>
-            <Table.Cell>
-              Use dockerfile and docker-compose to create containers for
-              services
-            </Table.Cell>
-          </Table.Row>
-        </Table.Body>
-      </Table>
-    </div>
-  </div>
-);
+            <Table.Body>
+              {this.skillsets.map(skillset => (
+                <Table.Row>
+                  <Table.Cell>{skillset.skill}</Table.Cell>
+                  <Table.Cell>{skillset.description}</Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default ResumeOtherBackendSkills;
