9e3f5b9db57eb8aaa9fadda790e196dd9da37f4d
roy1210 <rswkure@gmail.com>
2019-09-26 12:31:58 +0800

    Refactoring resume folder

--- src/components/Resume/ResumePage.js
+++ src/components/Resume/ResumePage.js
@@ -1,40 +1,40 @@
-import React, { Component } from "react";
-import { Grid, Cell } from "react-mdl";
-import ResumeEducations from "./ResumeEducations";
-import ResumeExperiences from "./ResumeExperiences";
-import ResumeSkills from "./ResumeSkills";
-import ResumeIntroduction from "./ResumeIntroduction";
-import AppFooter from "../AppFooter";
-import InBarcerona from "../../img/BarceronaProfile.jpg";
-import "../../CSS/Resume.css";
+import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+import ResumeEducations from './ResumeEducation/ResumeEducations';
+import ResumeExperiences from './ResumeExperience/ResumeExperiences';
+import ResumeSkills from './ResumeSkills/ResumeSkills';
+import ResumeIntroduction from './ResumeIntroduction';
+import AppFooter from '../AppFooter';
+import InBarcerona from '../../img/BarceronaProfile.jpg';
+import '../../CSS/Resume.css';
 
 class ResumePage extends Component {
   render() {
     return (
-      <div className="Resume">
+      <div className='Resume'>
         <Grid>
           <Cell col={4} phone={12} tablet={12}>
-            <div className="Resume-fade-in one Resume-avatar">
-              <img src={InBarcerona} alt="avatar" />
+            <div className='Resume-fade-in one Resume-avatar'>
+              <img src={InBarcerona} alt='avatar' />
             </div>
-            <div className="Resume-fade-in two">
+            <div className='Resume-fade-in two'>
               <ResumeIntroduction />
             </div>
           </Cell>
-          <Cell className="Resume-right-col" col={8} phone={12} tablet={12}>
-            <div className="Resume-fade-in three">
-              <div className="Resume-skills">
+          <Cell className='Resume-right-col' col={8} phone={12} tablet={12}>
+            <div className='Resume-fade-in three'>
+              <div className='Resume-skills'>
                 <ResumeSkills />
               </div>
-              <div className="Resume-education-job">
+              <div className='Resume-education-job'>
                 <ResumeEducations />
                 <ResumeExperiences />
               </div>
             </div>
           </Cell>
         </Grid>
-        <section style={{ marginBottom: "1em" }} />
-        <div className="Resume-footer">
+        <section style={{ marginBottom: '1em' }} />
+        <div className='Resume-footer'>
           <AppFooter />
         </div>
       </div>
