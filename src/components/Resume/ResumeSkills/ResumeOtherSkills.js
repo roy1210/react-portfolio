76cf6c6e9e41a167287229b277fd813ab2e670ff
roy1210 <rswkure@gmail.com>
2019-09-26 23:12:17 +0800

    Modified Website Optimization

--- src/components/Resume/ResumeSkills/ResumeOtherSkills.js
+++ src/components/Resume/ResumeSkills/ResumeOtherSkills.js
@@ -1,42 +1,40 @@
-import React, { Component } from "react";
-import { Table } from "semantic-ui-react";
+import React, { Component } from 'react';
+import { Table } from 'semantic-ui-react';
 
 class ResumeOtherBackendSkills extends Component {
   skillsets = [
     {
-      skill: "SSH",
-      description: "Remote linux server and generate ssh keys (DigitalOcean)"
+      skill: 'SSH',
+      description: 'Remote linux server and generate ssh keys (DigitalOcean)'
     },
     {
-      skill: "Website Optimization",
+      skill: 'Website Optimization',
+      description: 'Picture resize, Code splitting, Caching, Gzip'
+    },
+    {
+      skill: 'Progressive Web App',
+      description: 'Create PWA that works offline like a native mobile app'
+    },
+    {
+      skill: 'Secure',
       description:
-        "Picture resize, Code splitting, Load balancing, Caching and Code optimizations"
+        'HTTPS, Code secrets (.gitignore), Secure headers (Helmet), Encrypt'
     },
     {
-      skill: "Progressive Web App",
-      description: "Create PWA that works offline like a native mobile app"
-    },
-    {
-      skill: "Secure",
+      skill: 'Docker',
       description:
-        "HTTPS, Code secrets (.gitignore), Secure headers (Helmet), Encrypt"
+        'Use dockerfile and docker-compose to create containers for services'
     },
     {
-      skill: "Docker",
-      description:
-        "Use dockerfile and docker-compose to create containers for services"
-    },
-    {
-      skill: "Circle CI",
-      description:
-        "Implement CI/CD to keep code quality and avoid to deploy with any bug"
+      skill: 'Circle CI',
+      description: 'Implement CI/CD to avoid to deploy with any bug'
     }
   ];
   render() {
     return (
-      <div className="Resume-other-than-that">
-        <h5 className="Resume-backend-other">Other than above</h5>
-        <div style={{ fontSize: "1.1rem", margin: "0.3rem" }}>
+      <div className='Resume-other-than-that'>
+        <h5 className='Resume-backend-other'>Other than above</h5>
+        <div style={{ fontSize: '1.1rem', margin: '0.3rem' }}>
           <Table celled inverted>
             <Table.Header>
               <Table.Row>
